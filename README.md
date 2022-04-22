5a056fb6b0790520fa82465172c9c9c4e2f73645
AlxBzC <alexbaez78@gmail.com>
2022-04-22 11:38:48 -0500

    Add Proyect

---
+++ README.md
@@ -0,0 +1,10 @@
+# Responsive-web
+
+This is a web page designed in a responsive way, send email and link to social media.
+
+Basic web page designed with HTML for the structure, CSS for styles, JavaScript and JQuery for animation effects, PHP for sending email together with XAMPP as local host, responsive design with NORMALIZE for mobile devices and PC.
+
+/**** I hope you like!!! ****/
+
+
+
---
+++ index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="Coca cola es la bebida bla bla bla bla">
+		<meta name="keywords" content="bebida, gaseosa, cola">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<title>
+			Inicio - Coca Cola
+		</title>
+		<link rel="shortcut icon" href="img/favicon.png">
+		<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="style.css">
+		<script src="https://kit.fontawesome.com/37ef4fca07.js" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<a href="index.php">
+					<img src="img/logo.svg" class="logo">
+				</a>
+				<nav>
+					<a href="#inicio">Inicio</a>
+					<a href="#nosotros">Nosotros</a>
+					<a href="#servicios">Servicios</a>
+					<a href="#galeria">Galería</a>
+					<a href="#contactenos">Contáctenos</a>
+				</nav>
+				<a href="#" class="hamb"><i class="fas fa-bars"></i></a>
+			</div>
+		</header>
+		<main>
+			<section id="inicio">
+				<img src="img/bannerprincipal.jpg">
+				<div class="bloque-inicio">
+					<h1>Bienvenidos a Coca Cola</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+					</p>
+					<a href="#nosotros" class="boton boton-rojo">Ver más</a>
+				</div>
+			</section>
+			<section id="nosotros" class="seccion">
+				<div class="container">
+					<p>Nam accumsan tellus tortor, at aliquet est sodales vitae. Praesent posuere nunc eget odio dapibus, sed cursus mauris tincidunt. Donec accumsan arcu et urna eleifend sodales. Sed at neque non leo tincidunt semper. In ligula enim, rutrum sit amet Lorem eu, congue dapibus nisi.</p> 
+				</div>
+			</section>
+			<section id="servicios"  class="seccion">
+				<div class="container">
+					<div class="row">
+						<div class="columna columna-33 columna-mobile-100">
+							<div class="bloque-servicio">
+								<div class="bloque-img-servicio cuadrado-perfecto">
+									<img src="img/servicio1.jpg">
+								</div>
+								<div class="bloque-contenido-servicio">
+									<h3>Servicio 1</h3>
+									<p>Nam accumsan tellus tortor, at aliquet est sodales vitae. Praesent posuere nunc eget odio dapibus, sed cursus mauris tincidunt.</p>
+									<a href="#" class="boton boton-blanco">Ver más</a>
+								</div>
+							</div>
+						</div>
+						<div class="columna columna-33 columna-mobile-100">
+							<div class="bloque-servicio">
+								<div class="bloque-img-servicio cuadrado-perfecto">
+									<img src="img/servicio2.jpg">
+								</div>
+								<div class="bloque-contenido-servicio">
+									<h3>Servicio 2</h3>
+									<p>Nam accumsan tellus tortor, at aliquet est sodales vitae. Praesent posuere nunc eget odio dapibus, sed cursus mauris tincidunt.</p>
+									<a href="#" class="boton boton-blanco">Ver más</a>
+								</div>
+							</div>
+						</div>
+						<div class="columna columna-33 columna-mobile-100">
+							<div class="bloque-servicio">
+								<div class="bloque-img-servicio cuadrado-perfecto">
+									<img src="img/servicio3.jpg">
+								</div>
+								<div class="bloque-contenido-servicio">
+									<h3>Servicio 3</h3>
+									<p>Nam accumsan tellus tortor, at aliquet est sodales vitae. Praesent posuere nunc eget odio dapibus, sed cursus mauris tincidunt.</p>
+									<a href="#" class="boton boton-blanco">Ver más</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section id="galeria">
+				<div class="container-fluid">
+					<div class="row">
+						<div class=" columna-33 columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio1.jpg">
+								<h4>Imagen1</h4>
+							</div>
+						</div>
+						<div class=" columna-33   columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio2.jpg">
+								<h4>Imagen2</h4>
+							</div>	
+						</div>
+						<div class=" columna-33  columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio3.jpg">
+								<h4>Imagen3</h4>
+							</div>		
+						</div>
+						<div class=" columna-33  columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio1.jpg">
+								<h4>Imagen4</h4>
+							</div>	
+						</div>
+						<div class=" columna-33  columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio2.jpg">
+								<h4>Imagen5</h4>
+							</div>	
+						</div>
+						<div class=" columna-33  columna-mobile-50">
+							<div class="cuadrado-perfecto">
+								<img src="img/servicio3.jpg">
+								<h4>Imagen6</h4>
+							</div>	
+						</div>
+					</div>
+				</div>
+			</section>
+			<section id="contactenos" class="seccion">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.9189894441643!2d-77.2860661860511!3d1.216547262330085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e2ed5bff0377a35%3A0x31fcfb3ad3b4e372!2sCC.%20Bombona!5e0!3m2!1ses!2sco!4v1650600249107!5m2!1ses!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				<div class="container-fluid">    
+					<div class="row">
+						<div class="columna columna-41 columna-mobile-100 empujar-58 empujar-mobile-0 sinpadding-mobile">
+							<form action="index.php" method="post">
+								<div class="form-block">
+									<input type="text" name="nombre" class="form-control" placeholder="Nombre">
+								</div>
+								<div class="form-block">
+									<input type="email" name="email" class="form-control"  placeholder="Email">
+								</div>
+								<div class="form-block">
+									<textarea name="mensaje"  placeholder="Mensaje"></textarea>
+								</div>
+								<div class="form-block bloque-ultimo">
+									<input type="submit" class="boton boton-negro" value="Enviar">
+								</div>
+								<?php
+
+									if($_SERVER["REQUEST_METHOD"] == "POST")
+									{
+										$nombre = $_POST["nombre"] ;
+										$email = $_POST["email"] ;
+										$mensaje = $_POST["mensaje"] ;
+
+										if(isset($nombre))
+										{
+											if(isset($email))
+											{
+												if(isset($mensaje))
+												{
+													$para = "";
+													$asunto = "Esto es una prueba";
+													$cuerpo = $nombre."\n".$email."\n".$mensaje;
+													$adicional = "From: alexbaez78@gmail.com";
+
+													mail($para,$asunto,$cuerpo,$adicional);
+												?>
+													<p>Envio exitoso</p>
+												<?php
+												}
+											}
+										}
+									}
+								?>
+							</form>
+
+						</div>
+					</div>
+				</div>
+
+			</section>
+		</main>
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="columna columna-25 columna-mobile-100">
+						<img src="img/logo-blanco.png" class="logo-footer">
+						<p>
+							Nam accumsan tellus tortor, at aliquet est sodales vitae. Praesent posuere nunc eget odio dapibus, sed cursus mauris tincidunt.
+						</p>
+					</div>
+					<div class="columna columna-25 columna-mobile-100">
+						<h3>
+							Temas relacionados
+						</h3>
+						<ul>
+							<li><a href="#">Tema 1</a></li>
+							<li><a href="#">Tema 2</a></li>
+							<li><a href="#">Tema 3</a></li>
+						</ul>
+					</div>
+					<div class="columna columna-25 columna-mobile-100">
+						<h3>
+							Datos de contcto
+						</h3>
+						<ul>
+							<li>alexbaez78@gmail.com</li>
+							<li>+573184478888</li>
+							<li>Bombona - San Juan de Pasto<br>Nariño - Colombia</li>
+						</ul>
+					</div>
+					<div class="columna columna-25 columna-mobile-100">
+						<h3>
+							Redes Sociales
+						</h3>
+						<ul class="redes">
+							<li>
+								<a href="https://www.facebook.com/Coca-Cola/" target="_blank">
+									<i class="fab fa-facebook-square"></i>
+								</a>
+							</li>
+							<li>
+								<a href="https://twitter.com/CocaColaCol?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank">
+									<i class="fab fa-twitter-square"></i>
+								</a>
+							</li>
+							<li>
+								<a href="https://www.instagram.com/cocacolacol/?hl=es" target="_blank">
+									<i class="fab fa-instagram-square"></i>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="barra-footer">
+				&copy; Derechos Reservados - 2021
+			</div>
+		</footer>
+		<script src="js/jquery.js"></script>
+		<script src="js/funciones.js"></script>
+
+	</body>
+</html>
